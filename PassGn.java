181f7cf687db017dbde2da40c4cd8c8a3aa0e4dd
abdessamad ben cheraik <121764744+Bencabdessamad@users.noreply.github.com>
2023-07-12 17:32:53 +0100

    Update PassGn.java

--- PassGn.java
+++ PassGn.java
@@ -1,23 +1,26 @@
-import java.util.*;
-public class PassGn{
+import java.util.Random;
+
+public class PassGn {
     public static void main(String[] args) {
         int length = 10;
         System.out.println(geek_Password(length));
     }
-    static char[] geek_Password(int len){
-        System.out.println("Generating password using random() :    ");
-        System.out.println("Your password is :");
+
+    static String geek_Password(int len) {
+        System.out.println("Generating password using random():");
+        System.out.print("Your password is: ");
         String Capital_Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String Smalll_Chars = "abcdefghijklmopqrstuvwxyz";
+        String Smalll_Chars = "abcdefghijklmnopqrstuvwxyz";
         String Numbers = "0123456789";
         String Special_Chars = "!@#$%^&*()_+-={}[]:";
-        String values = Capital_Chars+Smalll_Chars+Numbers+Special_Chars;
+        String values = Capital_Chars + Smalll_Chars + Numbers + Special_Chars;
         Random rand = new Random();
-        char[] password = new char[len];
-        for(int i=0;i<len;i++){
-            password[i] = values.charAt(rand.nextInt(values.length()));
+        StringBuilder password = new StringBuilder(len);
+
+        for (int i = 0; i < len; i++) {
+            password.append(values.charAt(rand.nextInt(values.length())));
         }
-        return password;
+
+        return password.toString();
     }
-        
 }
